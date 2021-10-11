a35b53fc89ac98dc685dc76056dc8555fe96a180
malekkhasawneh <91069222+malekkhasawneh@users.noreply.github.com>
2021-10-11 12:41:46 +0300

    Delete lib directory

--- lib/features/random_number/presentation/bloc/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'random_number_bloc.dart';
-export 'random_number_event.dart';
-export 'random_number_state.dart';
--- lib/features/random_number/presentation/widget/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'loading_widget.dart';
-export 'show_message.dart';
-export 'random_display.dart';
-export 'random_controls.dart';
