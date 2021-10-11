de0a5714ee9c8fd1f5cbbe434f6e61f52e156669
malekkhasawneh <91069222+malekkhasawneh@users.noreply.github.com>
2021-10-11 12:22:35 +0300

    Delete lib directory

--- lib/features/random_number/domain/repository/random_number_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:tdd_full_project/core/error/failure.dart';
-import 'package:tdd_full_project/features/random_number/domain/entity/random_number.dart';
-
-abstract class RandomNumberRepository {
-  Future<Either<Failure, RandomNumber>> getConcreteRandomNumber(int number);
-
-  Future<Either<Failure, RandomNumber>> getRandomNumber();
-}
